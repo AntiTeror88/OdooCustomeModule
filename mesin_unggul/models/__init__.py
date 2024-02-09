fad858c8510cfeec502232bd999805f7a7792f49
AntiTeror88 <stevenmorizon123@gmail.com>
2024-02-09 15:20:37 +0700

    Update (09-02-2024)
    Updates module qc_shuttle dan mesin_unggul

---
+++ mesin_unggul/models/__init__.py
@@ -0,0 +1,6 @@
+from . import mesin_produksi
+from . import divisi
+from . import beam
+from . import location
+from . import deret_mesin
+from . import blok_mesin
--- qc_shuttle/__manifest__.py
+++ qc_shuttle/__manifest__.py
@@ -2,13 +2,14 @@
   'name': 'Snap QC Shuttle',
   'version': '14.0',
   'author': 'Steven Morison',
-  'summary': 'Pengecekkan Mesin Shuttle PT.Unggulrejo Wasono',
+  'summary': 'Pengecekkan Mesin Shuttle Unggulrejo Wasono',
   'author': 'Steven Morison, stevenmorizon123@gmail.com',
   'category': 'Manufacture',
   'depends': ['base', 'web', 'mesin_unggul'],
   'data': [
  
     'security/ir.model.access.csv',
+    'views/view_layout_shuttle.xml',
     'views/view_snap_qc.xml',
     'views/view_snap_qc_pop_up.xml',
     'views/css_loader.xml',
@@ -20,4 +21,3 @@
   'installable': True,
   'application': True,
 }
-
